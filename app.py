86fb8fd01093e8db582bfee840cfe5651dd689cb
Daniele Mancinelli <mancinelli.daniele@iisgalvanimi.edu.it>
2024-12-11 10:15:26 +0000

    ultima modifica lavoro completato

--- app.py
+++ app.py
@@ -2,22 +2,18 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Homepage
 @app.route('/')
 def home():
     return render_template('homepage.html')
 
-# Storia
 @app.route('/storia')
 def history():
     return render_template('storia.html')
 
-# Attrazioni
 @app.route('/attrazioni')
 def attractions():
     return render_template('attrazioni.html')
 
-# Eventi
 @app.route('/eventi')
 def events():
     return render_template('eventi.html')
